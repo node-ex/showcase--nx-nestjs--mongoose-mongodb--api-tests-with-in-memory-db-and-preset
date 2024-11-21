f97d6f63a0e254c1fd2d796dd00cb52b7c01cc72
Pavel Balashov <pavel.balashov1@gmail.com>
2024-11-21 11:21:04 +0100

    chore(app-nest-1): add a sample test for testing with in-memory DB

---
+++ apps/app-nest-1/src/index.with-in-memory-db.spec.ts
@@ -0,0 +1,22 @@
+import { debug as _debug } from 'debug';
+
+const debug = _debug('jest-mongodb:test');
+
+describe('Index', () => {
+  it('should pass', () => {
+    // https://github.com/shelfio/jest-mongodb#3-configure-mongodb-client
+    debug("process.env['MONGO_URL']", process.env['MONGO_URL']);
+    debug(
+      'globalThis.__MONGO_URI__',
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      (globalThis as any).__MONGO_URI__,
+    );
+    debug(
+      'globalThis.__MONGO_DB_NAME__',
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      (globalThis as any).__MONGO_DB_NAME__,
+    );
+
+    expect(true).toBe(true);
+  });
+});
